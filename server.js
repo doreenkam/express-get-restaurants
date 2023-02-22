9f8b663dcd72d6565a371019cec0e08a010e2d3c
Doreen Kamushinda <dkamushinda@expediagroup.com>
2023-02-22 13:17:21 +0000

    Added try / catch

--- server.js
+++ server.js
@@ -9,34 +9,53 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/restaurants', async (req, res) => {
-  const data = await Restaurant.findAll();
-  res.send(data);
+  try {
+    const data = await Restaurant.findAll();
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({ err: err.message });
+  }
 });
 
 app.get('/restaurants/:id', async (req, res) => {
-  const data = await Restaurant.findByPk(req.params.id);
-  res.send(data);
+  try {
+    const data = await Restaurant.findByPk(req.params.id);
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({ err: err.message });
+  }
 });
 
 app.post('/restaurants', async (req, res) => {
-  const data = await Restaurant.create(req.body);
-  res.send(data);
+  try {
+    const data = await Restaurant.create(req.body);
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({ err: err.message });
+  }
 });
 
 app.put('/restaurants/:id', async (req, res) => {
-  const id = req.params.id;
-  await Restaurant.update(req.body, {
-    where: { id },
-  });
-  const data = await Restaurant.findByPk(id);
-  res.send(data);
+  try {
+    const id = req.params.id;
+    await Restaurant.update(req.body, {
+      where: { id },
+    });
+    const data = await Restaurant.findByPk(id);
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({ err: err.message });
+  }
 });
 
 app.delete('/restaurants/:id', async (req, res) => {
-  const id = req.params.id;
-  await Restaurant.destroy({ where: { id } });
-  const remainder = await Restaurant.findAll();
-  res.send('Restaurant Deleted!');
+  try {
+    const id = req.params.id;
+    await Restaurant.destroy({ where: { id } });
+    res.status(200).send('Restaurant Deleted!');
+  } catch (error) {
+    res.status(500).send({ err: err.message });
+  }
 });
 
 app.listen(port, () => {

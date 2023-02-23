dbf42f41c74ad0fb0926593070c6cfe6f7e5800c
Doreen Kamushinda <dkamushinda@expediagroup.com>
2023-02-23 18:08:03 +0000

    Added field validators

---
+++ routes/restaurants.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const { Restaurant } = require('../models/index');
+const { check, validationResult } = require('express-validator');
+
+router.get('/', async (req, res) => {
+  try {
+    const data = await Restaurant.findAll();
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({ err: error.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const data = await Restaurant.findByPk(req.params.id);
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({ err: error.message });
+  }
+});
+
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty()
+      .trim()
+      .isLength([{ min: 10, max: 30 }]),
+    check('name', 'Name length is invalid').isLength({ min: 10, max: 30 }),
+    check('location', 'Location is required').not().isEmpty().trim(),
+    check('cuisine', 'Cuisine is required').not().isEmpty().trim(),
+  ],
+  async (req, res) => {
+    try {
+      let errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
+      } else {
+        const data = await Restaurant.create(req.body);
+        res.status(200).send(data);
+      }
+    } catch (error) {
+      res.status(500).send({ err: error.message });
+    }
+  }
+);
+
+router.put('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Restaurant.update(req.body, {
+      where: { id },
+    });
+    const data = await Restaurant.findByPk(id);
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({ err: error.message });
+  }
+});
+
+router.delete('/restaurants/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Restaurant.destroy({ where: { id } });
+    res.status(200).send('Restaurant Deleted!');
+  } catch (error) {
+    res.status(500).send({ err: error.message });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,62 +1,12 @@
 const express = require('express');
 const app = express();
-const { Restaurant } = require('./models/index');
 const { sequelize } = require('./db');
-
 const port = 3000;
+const restaurantsRoutes = require('./routes/restaurants');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/restaurants', async (req, res) => {
-  try {
-    const data = await Restaurant.findAll();
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send({ err: err.message });
-  }
-});
-
-app.get('/restaurants/:id', async (req, res) => {
-  try {
-    const data = await Restaurant.findByPk(req.params.id);
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send({ err: err.message });
-  }
-});
-
-app.post('/restaurants', async (req, res) => {
-  try {
-    const data = await Restaurant.create(req.body);
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send({ err: err.message });
-  }
-});
-
-app.put('/restaurants/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-    await Restaurant.update(req.body, {
-      where: { id },
-    });
-    const data = await Restaurant.findByPk(id);
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send({ err: err.message });
-  }
-});
-
-app.delete('/restaurants/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-    await Restaurant.destroy({ where: { id } });
-    res.status(200).send('Restaurant Deleted!');
-  } catch (error) {
-    res.status(500).send({ err: err.message });
-  }
-});
+app.use('/restaurants', restaurantsRoutes);
 
 app.listen(port, () => {
   sequelize.sync();
